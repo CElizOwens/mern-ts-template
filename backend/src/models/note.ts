ec384bdc1ca55654347b277535097d6c5dcdaedf
CElizOwens <owens.christina@gmail.com>
2023-09-22 13:00:02 -0700

    Full stack template with Bootstrap installed

--- backend/src/models/note.ts
+++ backend/src/models/note.ts
@@ -1,10 +1,13 @@
-import { InferSchemaType, model, Schema } from "mongoose";
+import { InferSchemaType, model, Schema } from 'mongoose';
 
-const noteSchema = new Schema({
+const noteSchema = new Schema(
+  {
     title: { type: String, required: true },
-    text: { type: String},
-}, { timestamps: true });
+    text: { type: String },
+  },
+  { timestamps: true },
+);
 
 type Note = InferSchemaType<typeof noteSchema>;
 
-export default model<Note>("Note", noteSchema);
+export default model<Note>('Note', noteSchema);
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,15 +1,15 @@
-import app from "./app";
-import mongoose from "mongoose";
-import env from "./util/validateEnv";
+import mongoose from 'mongoose';
+import app from './app';
+import env from './util/validateEnv';
 
-const PORT = env.PORT;
+const { PORT } = env;
 
-mongoose.connect(env.MONGO_CONNECTION_STRING)
-    .then(() => {
-        console.log("Mongoose connected");
-        app.listen(PORT, () => {
-            console.log("Server running on port " + PORT);
-        });
-    })
-    .catch(console.error);
-
+mongoose
+  .connect(env.MONGO_CONNECTION_STRING)
+  .then(() => {
+    console.log('Mongoose connected');
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
+  })
+  .catch(console.error);
